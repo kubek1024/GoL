efc42e61cf8580bff399ebfa6818815c9e8333d1
Michałll Buczek <m.buczek.pl@gmail.com>
2013-05-13 13:46:52 +0200

    pierwsze zycie

--- zycie.cpp
+++ zycie.cpp
@@ -6,16 +6,16 @@ Zycie::Zycie(QWidget *parent) :
     memset(&tablicaKomorek, false, sizeof(99));
 
 
-    for(int k = 0; k <= 99; k++) {
-        for(int j = 0; j <= 99; j++) {
+    for(int k = 0; k < 100; k++) {
+        for(int j = 0; j < 100; j++) {
             tablicaKomorek[k][j] = false;
         }
     }
 
-    tablicaKomorek[1][0]=true;
     tablicaKomorek[1][1]=true;
     tablicaKomorek[1][2]=true;
-    tablicaKomorek[1][3]=true;
+    tablicaKomorek[2][3]=true;
+    tablicaKomorek[2][4]=true;
 
 
 }
@@ -42,9 +42,9 @@ void Zycie::malujPiksele(QPainter &p)
     double szerokosc = (double)width()/100;
     double wysokosc= (double)height()/100;
 
-    for(int i=1; i<100;i++)
+    for(int i=0; i<100;i++)
     {
-        for(int j=1; j<100;j++)
+        for(int j=0; j<100;j++)
         {
             if(tablicaKomorek[i][j]==true)
             {
